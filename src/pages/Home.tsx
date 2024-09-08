ef76dfafc626f1c3559c1f0d76f375844cc5218a
Amogh Harish <amogh@aharish.xyz>
2024-09-08 22:15:47 +0530

    doc: Bump version to v0.9.807
    Signed-off-by: Amogh Harish <amogh@aharish.xyz>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -271,7 +271,7 @@ const Home: React.FC = () => {
         }
         <IonActionSheet
           trigger='openActionsSheet'
-          header={isPlatform('capacitor') && appInfo! ? appInfo.name + ' v' + appInfo.version + '.' + appInfo.build + ' pre-release' : 'Duet v0.9.805 pre-release'}
+          header={isPlatform('capacitor') && appInfo! ? appInfo.name + ' v' + appInfo.version + '.' + appInfo.build + ' pre-release' : 'Duet v0.9.807 pre-release'}
           buttons={[
             {
               text: 'Settings',
