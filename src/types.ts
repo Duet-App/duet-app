77b9e7201075f3eae1af21daa85b1940ba777d7f
Amogh Harish <amogh@aharish.xyz>
2024-09-21 22:36:40 +0530

    refactor(type): Add new types for Project, Task
    Signed-off-by: Amogh Harish <amogh@aharish.xyz>

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+interface Task extends PouchDB.Core.ExistingDocument<{}> {
+  title: string,
+  description?: string,
+  type: 'task',
+  status: 'Todo' | 'Next' | 'Done' | 'Cancelled' | 'Waiting',
+  due_date?: string,
+  scheduled_date?: string,
+  project_id?: PouchDB.Core.DocumentId,
+  timestamps: {
+    created: string,
+    updated: string,
+    completed?: string
+  }
+}
+
+interface Project extends PouchDB.Core.ExistingDocument<{
+  title: string,
+  description: string,
+  type: string,
+  status: string,
+  timestamps: {
+    created: string,
+    updated: string,
+    completed?: string,
+    archived?: string
+  }
+}> {}
+
+type TaskItemProps = {
+  task: Task,
+  updateFn: () => void,
+  project?: Project
+}
