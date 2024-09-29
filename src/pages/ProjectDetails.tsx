81439e0b9d3c688fe248b732490a2f8ff1e9c023
Amogh Harish <amogh@aharish.xyz>
2024-09-29 23:21:59 +0530

    feat(core): Add support to archive project
    Signed-off-by: Amogh Harish <amogh@aharish.xyz>

--- src/pages/ProjectDetails.tsx
+++ src/pages/ProjectDetails.tsx
@@ -1,11 +1,11 @@
-import { IonActionSheet, IonAlert, IonBackButton, IonBackdrop, IonButton, IonButtons, IonCheckbox, IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonPage, IonTextarea, IonToolbar, isPlatform, useIonRouter, useIonViewDidEnter } from "@ionic/react"
+import { IonActionSheet, IonAlert, IonBackButton, IonBackdrop, IonButton, IonButtons, IonCheckbox, IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonPage, IonText, IonTextarea, IonToolbar, isPlatform, useIonRouter, useIonViewDidEnter } from "@ionic/react"
 import { RouteComponentProps } from "react-router"
 import PouchDB from "pouchdb"
 import PouchFind from "pouchdb-find"
 import CordovaSqlite from "pouchdb-adapter-cordova-sqlite"
 import { useEffect, useRef, useState } from "react"
 import { formatDistance } from "date-fns"
-import { add, checkmark, checkmarkSharp, chevronDownSharp, closeSharp, documentTextSharp, ellipsisVerticalSharp, trashSharp } from "ionicons/icons"
+import { add, archiveSharp, checkmark, checkmarkSharp, chevronDownSharp, closeSharp, documentTextSharp, ellipsisVerticalSharp, trashSharp } from "ionicons/icons"
 import TaskItem from "../components/Tasks/TaskItem"
 import NoteItem from "../components/Notes/NoteItem"
 import Markdown from "react-markdown"
@@ -149,6 +149,27 @@ const ProjectDetailsPage: React.FC<ProjectDetailsPageProps> = ({match}) => {
     }
   }
 
+  const archiveProject = async () => {
+    try {
+      const timestamp = new Date().toISOString()
+      const updateProjectResponse = await db.put({
+        ...project,
+        status: project.status == "Archived" ? "In progress" : "Archived",
+        timestamps: {
+          ...project.timestamps,
+          updated: timestamp,
+          archived: project.status == "In progress" ? timestamp : null
+        }
+      })
+      if(updateProjectResponse.ok) {
+        const newProjectResponse = await db.get(project._id)
+        setProject(newProjectResponse)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <IonPage>
       <IonBackdrop
@@ -168,6 +189,14 @@ const ProjectDetailsPage: React.FC<ProjectDetailsPageProps> = ({match}) => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        {
+          project && project.status == "Archived"
+          ?  <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '4px', padding: '4px', textAlign: 'center', background: 'var(--ion-color-light-shade)', color: 'var(--ion-color-medium)', fontSize: '0.8rem', lineHeight: 1}}>
+            <IonIcon style={{fontSize: '0.79rem'}} icon={archiveSharp} color="medium"></IonIcon>
+            <IonLabel style={{lineHeight: 1.2}}>Archived</IonLabel>
+          </div>
+          : null
+        }
         <div style={{padding: '16px 16px 0'}}>
           {/* <h3>{project.title}</h3> */}
           <Title title={project.title} update={updateProjectTitle} />
@@ -267,6 +296,13 @@ const ProjectDetailsPage: React.FC<ProjectDetailsPageProps> = ({match}) => {
             </small>
             : null
           }
+          {
+            (project.timestamps && project.timestamps.archived)
+            ? <small style={{display: 'block', marginBottom: 8, color: 'var(--ion-color-medium)'}}>
+              Archived: {(project.timestamps && project.timestamps.archived) ? formatDistance(project.timestamps.archived, Date.now()) + ' ago' : ''}
+            </small>
+            : null
+          }
         </div>
 
         <IonFab ref={fabRef} onClick={() => {fabRef.current?.activated ? setOverlayVisible(true) : setOverlayVisible(false)}} slot='fixed' vertical='bottom' horizontal='end'>
@@ -288,6 +324,13 @@ const ProjectDetailsPage: React.FC<ProjectDetailsPageProps> = ({match}) => {
           header='Project actions'
           buttons={[
             {
+              text: project.status != "Archived" ? 'Archive' : 'Unarchive',
+              icon: archiveSharp,
+              data: {
+                action: 'archive-project'
+              }
+            },
+            {
               text: 'Delete',
               icon: trashSharp,
               role: 'destructive',
@@ -306,6 +349,8 @@ const ProjectDetailsPage: React.FC<ProjectDetailsPageProps> = ({match}) => {
           onDidDismiss={({detail}) => {
             if(detail.data?.action == 'delete-project') {
               deleteProjectConfirmation.current?.present()
+            } else if(detail.data?.action == 'archive-project') {
+              archiveProject()
             }
           }}
         ></IonActionSheet>
